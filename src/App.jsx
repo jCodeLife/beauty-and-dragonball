60f77dcaef2464d4f06856253f36faaf7efe5a55
jCodeLife <bingjin.liao@outlook.com>
2022-04-16 18:10:16 +0800

    feat: 实现跑步

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { createRef, useRef } from "react"
 function App() {
   // useKeyboard用于监控当前按键
   const key = useKeyboard()
+  console.log(key);
   const characterRef = createRef()
   //声明motion，用于表示当前角色应该对应的动画，默认为站立idle
   let motion = "idle";
@@ -15,31 +16,26 @@ function App() {
   if (key === "s") {
     motion = "walkingBackwards"
   }
-  // // 左
-  // if (key === "a") {
-  //   motion = "leftTurn"
-  // }
-  // // 右
-  // if (key === "d") {
-  //   motion = "rightTurn"
-  // }
+  // 按下w和e时，开始跑
+  if (key === "w e") {
+    motion = "running"
+  }
 
   // useLoop 帧循环勾子
   useLoop(() => {
-    characterRef.current.moveForward(-2)
+    characterRef.current.moveForward(-4)
   }, key === "w");
   useLoop(() => {
     characterRef.current.moveForward(1.8)
   }, key === "s");
-  // useLoop(() => {
-  //   characterRef.current.moveRight(-2);
-  // }, key === "d");
-  // useLoop(() => {
-  //   characterRef.current.moveRight(2);  
-  // }, key === "a");
+  useLoop(() => {
+    characterRef.current.moveForward(-10)
+  }, key === "w e");
 
   return (
     <World>
+      <Skybox texture="skybox.jpg" />
+
       <ThirdPersonCamera active mouseControl>
         <Model
           ref={characterRef}
@@ -48,7 +44,8 @@ function App() {
           animations={{
             idle: "idle.fbx",
             walking: "walking.fbx",
-            walkingBackwards: "walking-backwards.fbx"
+            walkingBackwards: "walking-backwards.fbx",
+            running: "running.fbx"
           }}
           animation={motion}
           scale={1}
@@ -60,7 +57,12 @@ function App() {
         scale={40}
         physics="map"
       />
-      <Skybox texture="skybox.jpg" />
+
+      <Model
+        src="dragon_ball/scene.gltf"
+        scale={10}
+        physics="character"
+      />
     </World >
   )
 }
